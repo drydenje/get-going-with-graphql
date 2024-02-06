3237efd8c9d0f53f477d133c3ae941771e1adfaf
Jeremy <jeremy.dryden@gmail.com>
2024-02-05 23:03:21 -0500

    trying to fix dotenv/config issue, variable undefined

--- server/src/graphql/dataSources/JsonServerApi.js
+++
@@ -1,7 +0,0 @@
-import { RESTDataSource } from "@apollo/datasource-rest";
-
-class JsonServerApi extends RESTDataSource {
-  //
-}
-
-export default JsonServerApi;
--- server/src/index.mjs
+++ server/src/index.mjs
@@ -2,10 +2,16 @@ import { ApolloServer } from "apollo-server";
 
 import resolvers from "./graphql/resolvers.mjs";
 import typeDefs from "./graphql/typeDefs.mjs";
-
+import JsonServerApi from "./graphql/dataSources/JsonServerApi.mjs";
+console.log("URL:", process.env.REST_API_BASE_URL);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  dataSources: () => {
+    return {
+      jsonServerApi: new JsonServerApi(),
+    };
+  },
 });
 
 server.listen().then(({ url }) => {
