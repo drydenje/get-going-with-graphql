d70aea004d4ea500419d26da23ddeae64f081e22
Jeremy <jeremy.dryden@gmail.com>
2024-03-27 23:13:37 -0400

    working on apollo-server-express

--- server/src/graphql/typeDefs.mjs
+++ server/src/graphql/typeDefs.mjs
@@ -1,4 +1,5 @@
-import gql from "graphql-tag";
+// import gql from "graphql-tag";
+import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
   directive @unique(
--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,32 +1,84 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+// import { ApolloServer } from "@apollo/server";
+// import { expressMiddleware } from "@apollo/server/express4";
+// import cors from "cors";
+// import express from "express";
+// import { startStandaloneServer } from "@apollo/server/standalone";
 import resolvers from "./graphql/resolvers.mjs";
 import typeDefs from "./graphql/typeDefs.mjs";
-import JsonServerApi from "./graphql/dataSources/JsonServerApi.mjs";
+// import JsonServerApi from "./graphql/dataSources/JsonServerApi.mjs";
 
-// This is for the 'unique' directive, need to update it for apollo-v4
-// import UniqueDirective from "./graphql/directives/UniqueDirective.mjs";
+// npm install @apollo/server express graphql cors
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
 
+// Required logic for integrating with Express
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
+
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+// Ensure we wait for our server to start
+await server.start();
 
-const { url } = await startStandaloneServer(server, {
-  context: async ({ req }) => {
-    const { cache } = server;
-    const token = req.headers.token;
-    return {
-      dataSources: {
-        jsonServerApi: new JsonServerApi({ cache, token }),
-      },
-      // This is for the 'unique' directive, need to update it for apollo-v4
-      // schemaDirectives: {
-      //   unique: UniqueDirective,
-      // },
-    };
-  },
-  listen: { port: process.env.PORT || 4000 },
-});
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
 
-console.log(`Server ready at ${url}`);
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
+
+// // This is for the 'unique' directive, need to update it for apollo-v4
+// // import UniqueDirective from "./graphql/directives/UniqueDirective.mjs";
+
+// const app = express();
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// // const { url } = await startStandaloneServer(server, {
+// //   context: async ({ req }) => {
+// //     const { cache } = server;
+// //     const token = req.headers.token;
+// //     return {
+// //       dataSources: {
+// //         jsonServerApi: new JsonServerApi({ cache, token }),
+// //       },
+// //       // This is for the 'unique' directive, need to update it for apollo-v4
+// //       // schemaDirectives: {
+// //       //   unique: UniqueDirective,
+// //       // },
+// //     };
+// //   },
+// //   listen: { port: process.env.PORT || 4000 },
+// // });
+
+// await server.start();
+
+// app.use("/graphql", cors(), express.json(), expressMiddleware(server));
+
+// // console.log(`Server ready at ${url}`);
